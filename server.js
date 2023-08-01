d44cfd35e3fc220fe825ed64070ffe1b4d964022
lpmwfx <69945037+lpmathiasendk@users.noreply.github.com>
2023-08-01 23:06:01 +0000

    fix(server.js): fix typo in variable declaration

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var http = require('http');
+var fs = require('fs'); // Add the fs module for file operations
 
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello, world!');
+  fs.readFile('index.html', function(error, content) {
+    if (error) {
+      res.writeHead(500);
+      res.end('Error loading index.html'); // Handle error if the file cannot be read
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content); // Send the content of index.html as the response
+    }
+  });
 });
 
 server.listen(3000, function() {
-    console.log('Server is listening on port 3000')
+  console.log('Server is listening on port 3000');
 });
